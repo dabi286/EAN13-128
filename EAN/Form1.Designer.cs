9e0c7e9dd9f7c9a42116961b874d46bea394d435
dave.visualB <nicka18chinese@gmail.com>
2025-12-16 20:44:27 +0800

    Add project files.

---
+++ EAN/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace EAN
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            button3 = new Button();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(1361, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(67, 34);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(117, 98);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(563, 569);
+            panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label12);
+            panel2.Controls.Add(label11);
+            panel2.Controls.Add(label10);
+            panel2.Controls.Add(label9);
+            panel2.Controls.Add(label8);
+            panel2.Controls.Add(label7);
+            panel2.Controls.Add(button3);
+            panel2.Location = new Point(757, 98);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(563, 569);
+            panel2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(0, 192, 0);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Century Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(76, 336);
+            button1.Name = "button1";
+            button1.Size = new Size(106, 38);
+            button1.TabIndex = 3;
+            button1.Text = "Print";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Code 128", 48F);
+            label1.Location = new Point(212, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(248, 63);
+            label1.TabIndex = 0;
+            label1.Text = "7793253002183";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Code 128", 48F);
+            label2.Location = new Point(212, 108);
+            label2.Name = "label2";
+            label2.Size = new Size(248, 63);
+            label2.TabIndex = 1;
+            label2.Text = "7793253002183";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Code 128", 48F);
+            label3.Location = new Point(212, 194);
+            label3.Name = "label3";
+            label3.Size = new Size(248, 63);
+            label3.TabIndex = 2;
+            label3.Text = "7793253002183";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Code 128", 48F);
+            label4.Location = new Point(212, 297);
+            label4.Name = "label4";
+            label4.Size = new Size(248, 63);
+            label4.TabIndex = 3;
+            label4.Text = "7793253002183";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Code 128", 48F);
+            label5.Location = new Point(212, 387);
+            label5.Name = "label5";
+            label5.Size = new Size(248, 63);
+            label5.TabIndex = 4;
+            label5.Text = "7793253002183";
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(0, 192, 0);
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Century Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(32, 233);
+            button3.Name = "button3";
+            button3.Size = new Size(106, 38);
+            button3.TabIndex = 5;
+            button3.Text = "Print";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Code 128", 48F);
+            label6.Location = new Point(212, 474);
+            label6.Name = "label6";
+            label6.Size = new Size(248, 63);
+            label6.TabIndex = 6;
+            label6.Text = "7793253002183";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Code 128", 48F);
+            label7.Location = new Point(148, 39);
+            label7.Name = "label7";
+            label7.Size = new Size(248, 63);
+            label7.TabIndex = 7;
+            label7.Text = "7793253002183";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Code 128", 48F);
+            label8.Location = new Point(148, 124);
+            label8.Name = "label8";
+            label8.Size = new Size(248, 63);
+            label8.TabIndex = 8;
+            label8.Text = "7793253002183";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Code 128", 48F);
+            label9.Location = new Point(148, 194);
+            label9.Name = "label9";
+            label9.Size = new Size(248, 63);
+            label9.TabIndex = 9;
+            label9.Text = "7793253002183";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Code 128", 48F);
+            label10.Location = new Point(148, 297);
+            label10.Name = "label10";
+            label10.Size = new Size(248, 63);
+            label10.TabIndex = 10;
+            label10.Text = "7793253002183";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Code 128", 48F);
+            label11.Location = new Point(148, 387);
+            label11.Name = "label11";
+            label11.Size = new Size(248, 63);
+            label11.TabIndex = 5;
+            label11.Text = "7793253002183";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Code 128", 48F);
+            label12.Location = new Point(148, 474);
+            label12.Name = "label12";
+            label12.Size = new Size(248, 63);
+            label12.TabIndex = 11;
+            label12.Text = "7793253002183";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1440, 857);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            WindowState = FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private Panel panel2;
+        private Button button1;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Button button3;
+        private Label label6;
+        private Label label12;
+        private Label label11;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+    }
+}
---
+++ EAN/Form1.cs
@@ -0,0 +1,10 @@
+namespace EAN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
